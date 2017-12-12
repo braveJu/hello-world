c8dd7639d32047ee5fed55f5443b89afc6e38703
DESKTOP-4644DH3\user <qkfmtpffhsk119@gmail.com>
2017-12-12 20:06:44 +0900

    메시지 추가

--- src/exercise/Rejava.java
+++ src/exercise/Rejava.java
@@ -3,7 +3,6 @@ package exercise;
 public class Rejava {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
-		
-		System.out.println("자바 이정도는 할 줄 알지 !");
+		System.out.println("abcdefg");
 	}
 }
